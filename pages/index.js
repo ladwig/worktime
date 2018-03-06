0094dae0f631279d070abb7383fa5152b7f8c0a1
ladwig <daniel.ladwig@hotmail.de>
2018-03-06 08:59:55 +0100

    I can log the duration of breaks now

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,9 @@ class Index extends Component {
     timePassed: 0,
     startTime: 0,
     endTime: 0,
+    startPause: 0,
+    endPause: 0,
+    durationPause: 0,
     list: [],
     buttonStart: 'block',
     buttonPause: 'none',
@@ -27,7 +30,7 @@ class Index extends Component {
   startTimer = () => {
     this.interval = setInterval(this.increaseTimer, 1000)
     this.setState({
-      startTime: new Date(),
+      startTime: new Date()
     })
     this.buttonPressed('start')
   }
@@ -35,6 +38,9 @@ class Index extends Component {
   //Used for the pause button
   pauseTimer = () => {
     clearInterval(this.interval)
+    this.setState({
+      startPause: new Date()
+    })
     this.buttonPressed('pause')
   }
 
@@ -42,6 +48,8 @@ class Index extends Component {
   continueTimer = () => {
     this.startTimer()
     this.buttonPressed('con')
+    const endPause = new Date()
+    this.pauser(endPause)
   }
 
   //Used for the stop button (saves the duration to the list)
@@ -52,7 +60,8 @@ class Index extends Component {
 
     this.setState({
       timePassed: 0,
-      endTime: x
+      endTime: x,
+      durationPause: 0
     })
 
     this.addToList(this.state.startTime, x)
@@ -63,6 +72,14 @@ class Index extends Component {
   //Get the 0 in front of single numbers (0-9)
   getNumber = deg => {
     return ('0' + deg).slice(-2)
+  }
+
+  //Logs the pause
+  pauser = (endPause) => {
+    let duration = moment(endPause).diff(this.state.startPause, 'ms')
+    this.setState({
+      durationPause: (duration + this.state.durationPause)
+    })
   }
 
   //Show/Hide buttons
@@ -100,8 +117,8 @@ class Index extends Component {
   //Method to add the startTime and endTime to the list array (state)
   addToList = (start, end) => {
     let listArray = this.state.list
-
-    listArray.push({ startTime: start, endTime: end, duration: this.state.duration})
+    let duration = moment(end).diff(start, 'ms')
+    listArray.push({ startTime: start, endTime: end, duration, durationPause: this.state.durationPause})
     this.setState({
       list: listArray
     })
@@ -127,8 +144,8 @@ class Index extends Component {
 
      <div>
       <main>
-        <div className='counter'>
-        {hours}:{minutes}:{seconds}
+        <div className="counter wrapper">
+          <div className="counter time">{hours}:{minutes}:{seconds}</div>
         </div>
         <div className="row first">
           <div className="start" onClick={this.startTimer}>Start</div>
@@ -152,14 +169,15 @@ class Index extends Component {
           {this.state.list.map(row => {
             const startTime = moment(row.startTime)
             const endTime = moment(row.endTime)
-            const durationM = endTime.diff(startTime, 'm')
-            const durationH = endTime.diff(startTime, 'h')
+            const duration = new Date(row.duration)
+            const durationPause = new Date(row.durationPause)
 
             return (
               <tr>
                 <td>{startTime.format('lll')}</td>
                 <td>{endTime.format('lll')}</td>
-                <td>{durationH + 'h ' +  durationM + 'm'}</td>
+                <td>{this.getNumber(duration.getHours() - 1) + ':' + this.getNumber(duration.getMinutes())}</td>
+
               </tr>
             )
           })}
@@ -172,7 +190,7 @@ class Index extends Component {
             height: 100%;
             background: #B3B3B3;
             z-index: -999;
-            font-family: Verdana, Geneva, sans-serif;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
           }
         `}</style>
 
@@ -181,21 +199,22 @@ class Index extends Component {
             margin: auto;
             margin-top: 25vh;
             width: 300px;
-            height 300px;
             background: #242325;
-            text-align: center;
           }
 
-          .counter {
+          .counter.wrapper {
+            height: 180px;
+          }
+
+          .counter.time {
             font-size: 55px;
             color: #C8C8C8;
-            padding-top: 30%;
+            text-align: center;
+            padding-top: 20%;
           }
 
-          table {
-            margin: auto;
-            text-align: center;
-            padding: 10px;
+          .row.first {
+            display: flex;
           }
 
           .start {
@@ -204,6 +223,7 @@ class Index extends Component {
             width: 100%;
             background: green;
             line-height: 60px;
+            text-align: center;
             display: ${this.state.buttonStart};
           }
 
@@ -213,6 +233,7 @@ class Index extends Component {
             width: 50%;
             background: orange;
             line-height: 60px;
+            text-align: center;
             display: ${this.state.buttonPause};
           }
 
@@ -222,6 +243,7 @@ class Index extends Component {
             width: 50%;
             background: lightgreen;
             line-height: 60px;
+            text-align: center;
             display: ${this.state.buttonCon};
           }
 
@@ -231,6 +253,7 @@ class Index extends Component {
             width: 50%;
             background: red;
             line-height: 60px;
+            text-align: center;
             display: ${this.state.buttonStop};
           }
 
@@ -241,6 +264,7 @@ class Index extends Component {
             background: lightgray;
             display: block;
             line-height: 60px;
+            text-align: center;
           }
 
           .popup {
@@ -256,6 +280,11 @@ class Index extends Component {
             display: ${this.state.popup};
           }
 
+          table {
+            margin: auto;
+            text-align: center;
+            padding: 10px;
+          }
 
           .close {
             color: #aaaaaa;
@@ -270,11 +299,6 @@ class Index extends Component {
             color: #000;
             text-decoration: none;
             cursor: pointer;
-        }
-
-        .row.first {
-          display: flex;
-          margin-top: 40px;
         }
 
         `}</style>
